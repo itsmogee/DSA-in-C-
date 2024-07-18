1600b635f43b0d2bd545fce2e1d75e1d24fdcad2
Mongezi Kgapola <mongezisamuel@gmail.com>
2024-07-19 01:07:30 +0200

    added : value at test function
    tests the valueAt method using some user input and hardcoded list values

--- Linked-List/main.cpp
+++ Linked-List/main.cpp
@@ -5,9 +5,12 @@ using namespace std;
 
 void testPushFront();
 
+void testValueAt();
+
 int main(int argc, char* argv[])
 {
     testPushFront();
+    testValueAt();
     return 0;
 }
 
@@ -26,4 +29,22 @@ void testPushFront()
         cout << linkedListTest.head->Key << endl;
         linkedListTest.head = linkedListTest.head->next;
     }
+}
+
+void testValueAt()
+{
+
+    List linkedListTest;
+
+    linkedListTest.pushFront(5);
+    linkedListTest.pushFront(10);
+    linkedListTest.pushFront(20);
+    linkedListTest.pushFront(6);
+
+    int index;
+    cout << "List Size : " << linkedListTest.getSize() << endl;
+    cout << "Input a index value " << endl;
+    cin >> index;
+    cout << endl
+         << "Value at : " << index << " = " << linkedListTest.valueAt(index) << endl;
 }
