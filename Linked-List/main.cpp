e4d47854161a8a739aabebb78023ea043f9768ac
Mongezi Kgapola <mongezisamuel@gmail.com>
2024-07-19 00:49:35 +0200

    added : pushFrontTest method
    tested pushFront method
    tested size

--- Linked-List/main.cpp
+++ Linked-List/main.cpp
@@ -1,6 +1,27 @@
+#include "list.h"
 #include <iostream>
 
-int main(int argc, char *argv[]) {
-  std::cout << "Hello World" << std::endl;
-  return 0;
+using namespace std;
+
+void testPushFront()
+{
+    List linkedListTest;
+
+    linkedListTest.pushFront(5);
+    linkedListTest.pushFront(10);
+    linkedListTest.pushFront(20);
+    linkedListTest.pushFront(6);
+
+    cout << "List Size : " << linkedListTest.getSize() << endl;
+
+    while (linkedListTest.head) {
+        cout << linkedListTest.head->Key << endl;
+        linkedListTest.head = linkedListTest.head->next;
+    }
+}
+
+int main(int argc, char* argv[])
+{
+    testPushFront();
+    return 0;
 }
